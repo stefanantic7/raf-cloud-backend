f4ae4662bd8a6641d5530873515f9a244fc60d60
antic <santic16@raf.rs>
2020-01-08 19:26:33 +0100

    Handle NoHandlerFoundException

---
+++ src/main/java/rs/raf/cloud/core/exception/NoHandlerFoundExceptionHandler.java
@@ -0,0 +1,19 @@
+package rs.raf.cloud.core.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class NoHandlerFoundExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+}
